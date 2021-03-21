8b4c0c151650a9ea4d8e086485b127e3bb559081
t_ita <brett.tetsuji@gmail.com>
2021-03-21 15:04:23 +0900

    first commit

---
+++ src/main/java/com/myexample/serverless/awsapigw/SpringAwsApigwNativeSampleApplication.java
@@ -0,0 +1,13 @@
+package com.myexample.serverless.awsapigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsApigwNativeSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsApigwNativeSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myexample/serverless/awsapigw/SpringAwsApigwNativeSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexample.serverless.awsapigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsApigwNativeSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
